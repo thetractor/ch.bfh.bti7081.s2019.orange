2ededfdc7aa4196c93432dfc70ad676de65764d8
Lars Peyer <lars.peyer@liip.ch>
2019-05-27 20:00:14 +0200

    Implement basic PatientDetail views

---
+++ src/main/java/ch/bfh/bti7081/ui/views/PatientDetail.java
@@ -0,0 +1,188 @@
+package ch.bfh.bti7081.ui.views;
+
+import ch.bfh.bti7081.Presenter.PatientPresenter;
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.HasUrlParameter;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import ch.bfh.bti7081.ui.MainLayout;
+import ch.bfh.bti7081.ui.components.FlexBoxLayout;
+import ch.bfh.bti7081.ui.components.navigation.bar.AppBar;
+import ch.bfh.bti7081.ui.layout.size.Bottom;
+import ch.bfh.bti7081.ui.layout.size.Horizontal;
+import ch.bfh.bti7081.ui.layout.size.Vertical;
+import ch.bfh.bti7081.ui.util.BoxShadowBorders;
+import ch.bfh.bti7081.ui.util.css.FlexDirection;
+import ch.bfh.bti7081.ui.util.css.FlexWrap;
+import model.entities.Patient;
+import org.bson.types.ObjectId;
+
+
+@Route(value = "patient-details", layout = MainLayout.class)
+@PageTitle("Patient Details")
+public class PatientDetail extends ViewFrame implements HasUrlParameter<String> {
+
+  public int RECENT_TRANSACTIONS = 4;
+
+  private Patient patient;
+  private PatientPresenter patientPresenter = new PatientPresenter();
+
+  @Override
+  protected void onAttach(AttachEvent attachEvent) {
+    super.onAttach(attachEvent);
+
+    AppBar appBar = initAppBar();
+    appBar.setTitle(patient.getName());
+    UI.getCurrent().getPage().setTitle(patient.getName());
+  }
+
+  @Override
+  public void setParameter(BeforeEvent beforeEvent, String patientIdString) {
+    setViewContent(createContent());
+    patient = patientPresenter.getPatient(new ObjectId(patientIdString));
+  }
+
+  private AppBar initAppBar() {
+    AppBar appBar = MainLayout.get().getAppBar();
+    appBar.setNaviMode(AppBar.NaviMode.CONTEXTUAL);
+    appBar.getContextIcon().addClickListener(
+        e -> UI.getCurrent().navigate(Patients.class));
+    return appBar;
+  }
+
+  private Component createContent() {
+    FlexBoxLayout content = new FlexBoxLayout(createLogoSection()
+//        createRecentTransactionsHeader(),
+//        createRecentTransactionsList(), createMonthlyOverviewHeader(),
+//        createMonthlyOverviewChart()
+        );
+    content.setFlexDirection(FlexDirection.COLUMN);
+    content.setMargin(Horizontal.AUTO, Vertical.RESPONSIVE_L);
+    content.setMaxWidth("840px");
+    return content;
+  }
+
+  private FlexBoxLayout createLogoSection() {
+//    Image image = DummyData.getLogo();
+//    image.addClassName(LumoStyles.Margin.Horizontal.L);
+//    UIUtils.setBorderRadius(BorderRadius._50, image);
+//    image.setHeight("200px");
+//    image.setWidth("200px");
+//
+//    availability = new ListItem(
+//        UIUtils.createTertiaryIcon(VaadinIcon.DOLLAR), "",
+//        "Availability");
+//    availability.setId("availability");
+//    availability.getPrimary().addClassName(LumoStyles.Heading.H2);
+//    availability.setDividerVisible(true);
+//    availability.setReverse(true);
+//
+//    bankAccount = new ListItem(
+//        UIUtils.createTertiaryIcon(VaadinIcon.INSTITUTION), "", "");
+//    bankAccount.setId("bankAccount");
+//    bankAccount.setDividerVisible(true);
+//    bankAccount.setReverse(true);
+//    bankAccount.setWhiteSpace(WhiteSpace.PRE_LINE);
+//
+//    updated = new ListItem(UIUtils.createTertiaryIcon(VaadinIcon.CALENDAR),
+//        "", "Updated");
+//    updated.setReverse(true);
+//
+//    FlexBoxLayout listItems = new FlexBoxLayout(availability, bankAccount,
+//        updated);
+//    listItems.setFlexDirection(FlexDirection.COLUMN);
+
+    FlexBoxLayout section = new FlexBoxLayout();
+    section.addClassName(BoxShadowBorders.BOTTOM);
+    section.setAlignItems(FlexComponent.Alignment.CENTER);
+//    section.setFlex("1", listItems);
+    section.setFlexWrap(FlexWrap.WRAP);
+    section.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+    section.setPadding(Bottom.L);
+    return section;
+  }
+
+//  private Component createRecentTransactionsHeader() {
+//    Label title = UIUtils.createH3Label("Recent Transactions");
+//
+//    Button viewAll = UIUtils.createSmallButton("View All");
+//    viewAll.addClickListener(
+//        e -> UIUtils.showNotification("Not implemented yet."));
+//    viewAll.addClassName(LumoStyles.Margin.Left.AUTO);
+//
+//    FlexBoxLayout header = new FlexBoxLayout(title, viewAll);
+//    header.setAlignItems(FlexComponent.Alignment.CENTER);
+//    header.setMargin(Bottom.M, Horizontal.RESPONSIVE_L, Top.L);
+//    return header;
+//  }
+//
+//  private Component createRecentTransactionsList() {
+//    Div items = new Div();
+//    items.addClassNames(BoxShadowBorders.BOTTOM,
+//        LumoStyles.Padding.Bottom.L);
+//
+//    for (int i = 0; i < RECENT_TRANSACTIONS; i++) {
+//      Double amount = DummyData.getAmount();
+//
+//      Label amountLabel = UIUtils.createAmountLabel(amount);
+//      if (amount > 0) {
+//        UIUtils.setTextColor(TextColor.SUCCESS, amountLabel);
+//      } else {
+//        UIUtils.setTextColor(TextColor.ERROR, amountLabel);
+//      }
+//
+//      ListItem item = new ListItem(DummyData.getLogo(),
+//          DummyData.getCompany(),
+//          UIUtils.formatDate(LocalDate.now().minusDays(i)),
+//          amountLabel);
+//
+//      // Dividers for all but the last item
+//      item.setDividerVisible(i < RECENT_TRANSACTIONS - 1);
+//      items.add(item);
+//    }
+//
+//    return items;
+//  }
+//
+//  private Component createMonthlyOverviewHeader() {
+//    Label header = UIUtils.createH3Label("Monthly Overview");
+//    header.addClassNames(LumoStyles.Margin.Vertical.L,
+//        LumoStyles.Margin.Responsive.Horizontal.L);
+//    return header;
+//  }
+//
+//  private Component createMonthlyOverviewChart() {
+//    Chart chart = new Chart(ChartType.COLUMN);
+//
+//    Configuration conf = chart.getConfiguration();
+//    conf.setTitle("");
+//    conf.getLegend().setEnabled(true);
+//
+//    XAxis xAxis = new XAxis();
+//    xAxis.setCategories("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+//        "Aug", "Sep", "Oct", "Nov", "Dec");
+//    conf.addxAxis(xAxis);
+//
+//    conf.getyAxis().setTitle("Amount ($)");
+//
+//    // Withdrawals and deposits
+//    ListSeries withDrawals = new ListSeries("Withdrawals");
+//    ListSeries deposits = new ListSeries("Deposits");
+//
+//    for (int i = 0; i < 8; i++) {
+//      withDrawals.addData(DummyData.getRandomInt(5000, 10000));
+//      deposits.addData(DummyData.getRandomInt(5000, 10000));
+//    }
+//
+//    conf.addSeries(withDrawals);
+//    conf.addSeries(deposits);
+//
+//    FlexBoxLayout card = new FlexBoxLayout(chart);
+//    card.setHeight("400px");
+//    return card;
+//  }
+}
--- src/main/java/ch/bfh/bti7081/ui/views/Patients.java
+++ src/main/java/ch/bfh/bti7081/ui/views/Patients.java
@@ -7,6 +7,7 @@ import ch.bfh.bti7081.ui.util.UIUtils;
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.DetachEvent;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
@@ -42,8 +43,8 @@ public class Patients extends ViewFrame implements RouterLayout {
         grid = new Grid<>(Patient.class);
         grid.setId("patients");
         grid.setColumns("name", "surname");
-//        grid.addSelectionListener(event -> event.getFirstSelectedItem()
-//                .ifPresent(this::viewDetails));
+        grid.addSelectionListener(event -> event.getFirstSelectedItem()
+                .ifPresent(this::viewDetails));
 //        grid.setDataProvider(
 //                DataProvider.ofCollection(DummyData.getBankAccounts()));
           grid.setSizeFull();
@@ -78,20 +79,7 @@ public class Patients extends ViewFrame implements RouterLayout {
     }
 
     @Override
-    protected void onAttach(AttachEvent attachEvent){
-        System.out.println("Attached!");
-        ObjectId objectId = (ObjectId) VaadinService.getCurrentRequest().getAttribute("doctorId");
-        System.out.println(objectId);
-        if(objectId == null){
-            // Redirects client to homepage if there is no doctorId in the session
-            // ToDo: update
-            // UI.getCurrent().navigate(Home.class);
-        }
-    }
-
-    @Override
     protected void onDetach(DetachEvent detachEvent){
-        System.out.println("Detached!");
     }
 //
 //    private Component createAvailability(BankAccount bankAccount) {
@@ -107,9 +95,9 @@ public class Patients extends ViewFrame implements RouterLayout {
 //        return label;
 //    }
 //
-//    private void viewDetails(BankAccount bankAccount) {
-//        UI.getCurrent().navigate(AccountDetails.class, bankAccount.getId());
-//    }
+    private void viewDetails(Patient patient) {
+        UI.getCurrent().navigate(PatientDetail.class, patient.getId().toString());
+    }
 
     /**
      * Load the data for the patient overview
