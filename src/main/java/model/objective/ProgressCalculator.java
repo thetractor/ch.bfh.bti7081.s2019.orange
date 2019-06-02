15cd6dadb55ffa3e6f43b8e683c93f91f7d78845
demg1 <gian.demarmels>
2019-06-02 20:07:00 +0200

    feat: added junit 5, added first test for progress calculation

--- src/main/java/model/objective/ProgressCalculator.java
+++ src/main/java/model/objective/ProgressCalculator.java
@@ -9,6 +9,6 @@ public class ProgressCalculator {
     public static double calculateProgress(List<Objective> objectives){
         double totalWeight = objectives.stream().mapToDouble(Objective::getWeight).sum();
         double currentWeightDone = objectives.parallelStream().mapToDouble(x -> (x.getProgress()/100) * x.getWeight()).sum();
-        return totalWeight * currentWeightDone;
+        return (100/totalWeight) * currentWeightDone;
     }
 }
