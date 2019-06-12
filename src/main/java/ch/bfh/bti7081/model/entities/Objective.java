d7d1aa255329433f41ed54b35f5bce0715b613fe
YannisSchmutz <yannis.schmutz@gmail.com>
2019-06-12 19:15:54 +0200

    refactor: finish comments for the model classes (sprint3-finalising)

--- src/main/java/ch/bfh/bti7081/model/entities/Objective.java
+++ src/main/java/ch/bfh/bti7081/model/entities/Objective.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.Objects;
 
 /**
+ * Implementation of the Objective entity
  * @author gian.demarmels@students.bf.ch
  * @author yannis.schmutz@students.bf.ch
  */
@@ -41,78 +42,157 @@ public class Objective implements IEntity {
         this.parent = parent;
     }
 
+    /**
+     * Empty constructor is needed in order to create an object out of the database
+     */
     public Objective(){
 
     }
 
+    /**
+     * Sets the entities ID
+     * This method is needed in order to create an object out of the database, it has to be public to do that.
+     *
+     * TODO: Encapsulate this so the method is not publicly accessible
+     *
+     * @param id ObjectId
+     */
     public void setId(ObjectId id) {
         this.id = id;
     }
 
+    /**
+     * Returns the entities Id
+     * @return ObjectId
+     */
     public ObjectId getId() {
         return id;
     }
 
+    /**
+     * Returns the due Date
+     * @return Date
+     */
     public Date getDueDate() {
         return new Date(dueDate.getTime());
     }
 
+    /**
+     * sets due date
+     * @param dueDate Date
+     */
     public void setDueDate(Date dueDate) {
         this.dueDate = new Date(dueDate.getTime());
     }
 
+    /**
+     * gets creator id
+     * @return ObjectId
+     */
     public ObjectId getCreatorId() {
         return creatorId;
     }
 
+    /**
+     * sets creator id
+     * @param creatorId ObjectId
+     */
     public void setCreatorId(ObjectId creatorId) {
         this.creatorId = creatorId;
     }
 
+    /**
+     * get objective content
+     * @return String
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * sets the content
+     * @param content String
+     */
     public void setContent(String content) {
         this.content = content;
     }
 
+    /**
+     * gets the objectives title
+     * @return String
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * sets the objectives title
+     * @param title String
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * returns progress
+     * @return double
+     */
     public double getProgress() {
         return progress;
     }
 
+    /**
+     * sets the progress
+     * @param progress double
+     */
     public void setProgress(double progress) {
         this.progress = progress;
     }
 
+    /**
+     * gets the weight
+     * @return double
+     */
     public double getWeight() {
         return weight;
     }
 
+    /**
+     * seth the weight
+     * @param weight double
+     */
     public void setWeight(double weight) {
         this.weight = weight;
     }
 
+    /**
+     * returns the parent of the objective
+     * @return ObjectId
+     */
     public ObjectId getParent() {
         return parent;
     }
 
+    /**
+     * sets the parent of the objective
+     * @param parent ObjectId
+     */
     public void setParent(ObjectId parent) {
         this.parent = parent;
     }
 
+    /**
+     * Get the id of the patient the objective is assigned to
+     * @return ObjectiveId
+     */
     public ObjectId getPatientId() {
         return patientId;
     }
 
+    /**
+     * sets the patients id
+     * @param patientId ObjectId
+     */
     public void setPatientId(ObjectId patientId) {
         this.patientId = patientId;
     }
